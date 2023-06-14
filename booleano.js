3c46fa8ca450d633bd165524745ec601eed72105
Waldir  Neto <50805130+waldirgarcia@users.noreply.github.com>
2023-06-14 13:31:10 -0300

    Add files via upload

---
+++ booleano.js
@@ -0,0 +1,31 @@
+//boolean, true or false.
+
+//const usuarioLogado = true;
+//const contaPaga = false;
+//truthy or falsy
+
+//0 => false 
+//1 => true
+
+//console.log(0 == false);
+//console.log("" == false);
+
+//null
+//undefined
+
+//null foi criado como objeto, que não é a forma tradicional  como ausência no JS , não foi ajustado em versões posteriores 
+//devido  número de aplicações que seriam quebradas , 
+
+let minhaVar;
+let varNull = null;
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
+
+
+//let numero = 3;
+//let texto = "waldir";
+//console.log(typeof numero);
+//console.log(typeof texto);
---
+++ consersao.js
@@ -0,0 +1,27 @@
+//tipos de dados 
+//booleanos
+//conversao implicita
+// == compara apenas o que é enviado 
+// ==== compara tipo de dado enviado e o dado enviado
+
+//const numero = 456; 
+// numeroString = "456";
+
+//console.log(numero == numeroString);
+
+//const numero = 456; 
+//const numeroString = "456";
+
+//console.log(numero + numeroString);
+//+ jogou os dados da primeiro variável na segunda , transformando em string 
+
+//conversão explicita 
+//Number  
+//String 
+
+const numero = 456; 
+const numeroString = "456";
+
+console.log(numero + Number (numeroString));
+
+//converter valor antes da string em Number com função Number N maiúscula 
